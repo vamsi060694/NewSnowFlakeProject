109ef9f3c72d15e41bb24246787ad1ea0e9cf7b5
vamsi060694 <vamsi1994@outlook.com>
2022-01-06 23:52:15 +0530

    exception handling

--- Adls_to_snowFlake.py
+++ Adls_to_snowFlake.py
@@ -1,31 +1,25 @@
-
-from snowflake.connector.pandas_tools import write_pandas
-from utils import snowflake_connection, downloading_loading, cs
-from utils import reading_snowflake_loading_df
-from utils import reading_snowflake_loading_df_1
-import snowflake.connector
+from utils import downloading_loading, cs, init_logger
+from utils import look_up_table
+from utils import look_up_tabl_1
 import sys
 
-
-
+logger = init_logger()
 
 if len(sys.argv) < 2:
-    sql = "select * from LOOK_UP_TABLE "
-    look_up = reading_snowflake_loading_df(cs, sql)
-    df = look_up.reset_index(drop=True)
-    #print(type(df))
-    downloading_loading(df)
-else:
-    for i in range(1,len(sys.argv)):
-        sql1 = "select * from LOOK_UP_TABLE where CONTAINER_NAME= ?"
-        look_up = reading_snowflake_loading_df_1(cs, sql1,sys.argv[i])
+    try:
+        sql = "select * from LOOK_UP_TABLE "
+        look_up = look_up_table(cs, sql)
         df = look_up.reset_index(drop=True)
         downloading_loading(df)
+    except Exception as e:
+        logger.error(e)
+else:
+    try:
+        for i in range(1, len(sys.argv)):
+            sql1 = "select * from LOOK_UP_TABLE where CONTAINER_NAME= ?"
+            look_up = look_up_tabl_1(cs, sql1, sys.argv[i])
+            df = look_up.reset_index(drop=True)
+            downloading_loading(df)
+    except Exception as e:
+        logger.error(e)
 cs.close()
-
-
-
-
-
-
-
--- utils.py
+++ utils.py
@@ -1,44 +1,71 @@
 import os
 import pandas as pd
 import snowflake
+import logging
 from azure.storage.filedatalake import DataLakeFileClient
 from snowflake.connector.pandas_tools import write_pandas
 
 snowflake.connector.paramstyle = '?'
 
 
+def init_logger():
+    logging.basicConfig(filename='log_file',format='%(asctime)s : %(levelname)s : %(message)s')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
 def downloading_gen2lake(container_name, file_name):
-    account_url = 'DefaultEndpointsProtocol=https;AccountName=projectgen2lake;AccountKey=mV8phW3sSwhrCct//+Dbd7ijnMAIeGakH+M6FU/aKtWSHdKq/m5KBvWWtaRD9B6mbrPezuBp6TuJ9mpmIoM7Og==;EndpointSuffix=core.windows.net'
-    file = DataLakeFileClient.from_connection_string(account_url, file_system_name=container_name, file_path=file_name)
-    print(container_name, file_name)
-    with open(r'C:\Users\Ant PC\PycharmProjects\NewSnowFlakeProject1\{}'.format(file_name), "wb") as my_file:
-        download = file.download_file()
-        download.readinto(my_file)
-    return my_file
+    global logger
+    logger = init_logger()
+    try:
+        account_url = 'DefaultEndpointsProtocol=https;AccountName=projectgen2lake;AccountKey=mV8phW3sSwhrCct//+Dbd7ijnMAIeGakH+M6FU/aKtWSHdKq/m5KBvWWtaRD9B6mbrPezuBp6TuJ9mpmIoM7Og==;EndpointSuffix=core.windows.net'
+        file = DataLakeFileClient.from_connection_string(account_url, file_system_name=container_name,
+                                                         file_path=file_name)
+        logger.debug('container_name:{},file_name:{}'.format(container_name, file_name))
+        with open(r'C:\Users\Ant PC\PycharmProjects\NewSnowFlakeProject1\{}'.format(file_name), "wb") as my_file:
+            download = file.download_file()
+            download.readinto(my_file)
+            logger.debug('{} downloaded from Gen2Lake'.format(row[1]))
+        return my_file
+    except Exception as e:
+        logger.error(e)
 
 
-def reading_snowflake_loading_df(cs, sql):
-    cs.execute(sql)
-    df = cs.fetch_pandas_all()
-    return df
+def look_up_table(cs, sql):
+    try:
+        cs.execute(sql)
+        df = cs.fetch_pandas_all()
+        return df
+    except Exception as e:
+        logger.error(e)
 
 
-def reading_snowflake_loading_df_1(cs, sql, a):
-    cs.execute(sql, [a])
-    df = cs.fetch_pandas_all()
-    return df
-
+def look_up_tabl_1(cs, sql, a):
+    try:
+        cs.execute(sql, [a])
+        df = cs.fetch_pandas_all()
+        return df
+    except Exception as e:
+        logger.error(e)
 
 def snowflake_connection():
-    ctx = snowflake.connector.connect(
-        user=access_details['user'],
-        password=access_details['password'],
-        account=access_details['account'],
-        warehouse=access_details['warehouse'],
-        database=access_details['database'],
-        schema=access_details['schema']
-    )
-    return ctx
+    global logger
+    try:
+        logger = init_logger()
+        ctx = snowflake.connector.connect(
+            user=access_details['user'],
+            password=access_details['password'],
+            account=access_details['account'],
+            warehouse=access_details['warehouse'],
+            database=access_details['database'],
+            schema=access_details['schema']
+        )
+        return ctx
+    except Exception as e:
+        logger.error(e)
+
 
 
 access_details = {'user': 'mysnowflake94',
@@ -50,18 +77,28 @@ access_details = {'user': 'mysnowflake94',
 
 
 def downloading_loading(x):
-    for row in x.values:
-        downloaded_file = downloading_gen2lake(row[0], row[1])
-        path = r'C:\Users\Ant PC\PycharmProjects\NewSnowFlakeProject1\{}'.format(row[1])
-        vendor_data_df = pd.read_csv(path, header=0)
-        vendor_data_df.reset_index(drop=True, inplace=True)
-        success, nchunks, nrows, _ = write_pandas(y, vendor_data_df, row[2])
-        print(success)
-        if os.path.exists(path):
-            os.remove(path)
-        else:
-            print("The file does not exist")
-    return
+    global row
+    try:
+        for row in x.values:
+            downloading_gen2lake(row[0], row[1])
+            path = r'C:\Users\Ant PC\PycharmProjects\NewSnowFlakeProject1\{}'.format(row[1])
+            file_df = pd.read_csv(path, header=0)
+            row_count = file_df.shape[0]
+            vendor_data_df = pd.read_csv(path, header=0)
+            vendor_data_df.reset_index(drop=True, inplace=True)
+            success, nchunks, nrows, _ = write_pandas(y, vendor_data_df, row[2])
+            logger.debug('{} status {}'.format(row[1],success))
+            if row_count == nrows:
+                logger.info('All rows loaded into SnowFlake')
+            else:
+                logger.info('few rows are missing in SnowFlake')
+            if os.path.exists(path):
+                os.remove(path)
+            else:
+                logger.info("The file does not exist")
+        return
+    except Exception as e:
+        logger.error(e)
 
 
 y = snowflake_connection()
